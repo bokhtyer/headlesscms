eae3048dd58e69277d6ab921616e3bb4fb161b22
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2024-12-16 13:06:15 +0600

    update

--- core/routes/index.ts
+++ core/routes/index.ts
@@ -1,23 +1,20 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import HelloWorldRouteController from '../controllers/HelloWorldRouteController';
-import WSController from '../controllers/WSController';
+import express, { Application, Request, Response, NextFunction } from "express";
+import bodyParser from "body-parser";
+import https from "https";
+import fs from "fs";
+import HelloWorldRouteController from "../controllers/HelloWorldRouteController";
+import WSController from "../controllers/WSController";
 var router = express.Router();
 import auth from "../../shared/middlewares/authMiddleware";
-import { ENV_CONFIG } from '../../config/environment';
+import { ENV_CONFIG } from "../../config/environment";
 
-
-
-router.get('/', function (req: Request, res: Response, next: NextFunction) {
-    res.render('index', { title: `Hello World from ${ENV_CONFIG.APP_NAME}` });
+router.get("/", function (req: Request, res: Response, next: NextFunction) {
+    res.render("index", { title: `Hello World from ${ENV_CONFIG.APP_NAME}` });
 });
-router.all(
-    '/ws/*',
-    auth(),
-    WSController.index
-);
-router.get('/content', function (req: Request, res: Response, next: NextFunction) {
-    res.render('index', { title: 'Hello World' });
+router.all("/ws/*", auth(), WSController.index);
+router.get("/content", function (req: Request, res: Response, next: NextFunction) {
+    res.render("index", { title: "Hello World" });
 });
-router.get('/api', HelloWorldRouteController.hola);
+router.get("/api", HelloWorldRouteController.hola);
 
 export default router;
